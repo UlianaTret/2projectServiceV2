cf8ed65c9300a9a57f09c52ef2b5e3471e65e849
UlianaTret <50988744+UlianaTret@users.noreply.github.com>
2023-02-12 06:53:14 +0300

    files js

---
+++ js/script.js
@@ -0,0 +1,27 @@
+if (window.matchMedia("(max-width: 768px)").matches) {
+	$(document).ready(function(){
+		$('.slider').slick({
+			arrows:true,
+			dots:true,
+			slidesToShow:1.1,
+			autoplay:false,
+			speed:1000,
+			// autoplaySpeed:800,
+			// responsive:[
+			// 	{
+			// 		breakpoint: 768,
+			// 		settings: {
+			// 			slidesToShow:2
+			// 		}
+			// 	},
+			// 	{
+			// 		breakpoint: 550,
+			// 		settings: {
+			// 			slidesToShow:1
+			// 		}
+			// 	}
+			// ]
+		});
+	});
+}
+
